897e02390c14f4c7b1ec51a14fba17273b35ce0c
om-garg <69619075+om-garg@users.noreply.github.com>
2022-07-02 21:27:46 +0530

    Search bar added

--- lib/model/notes_data.dart
+++ lib/model/notes_data.dart
@@ -7,7 +7,11 @@ import 'notes.dart';
 class NotesData extends ChangeNotifier {
   List<Notes> _notes = [];
 
-  NotesData(){
+  String _searchQuery = '';
+
+  String get searchQuery => _searchQuery;
+
+  NotesData() {
     fetchNotes();
   }
 
@@ -19,26 +23,38 @@ class NotesData extends ChangeNotifier {
     return _notes.length;
   }
 
+  void setSearchQuery(String val) {
+    _searchQuery = val;
+    notifyListeners();
+  }
+
+  List<Notes> getFilteredNotes() {
+    return _notes
+        .where((element) =>
+            element.title.toLowerCase().contains(_searchQuery.toLowerCase()) || element.content.toLowerCase().contains(_searchQuery.toLowerCase()))
+        .toList();
+  }
+
   void addNotes(Notes notes) {
     final note = Notes(
-      title: notes.title,
-      content: notes.content,
-      id: notes.id,
-      userid: notes.userid,
-      dateAdded: notes.dateAdded
-    );
+        title: notes.title,
+        content: notes.content,
+        id: notes.id,
+        userid: notes.userid,
+        dateAdded: notes.dateAdded);
     _notes.add(note);
     notifyListeners();
     NotesRepository.addNote(note);
   }
 
   void updateNotes(Notes note) {
-    int indexOfNote = _notes.indexOf(_notes.firstWhere((element) => element.id == note.id));
+    int indexOfNote =
+        _notes.indexOf(_notes.firstWhere((element) => element.id == note.id));
     _notes[indexOfNote] = note;
     notifyListeners();
-    NotesRepository.updateNote (note);
+    NotesRepository.updateNote(note);
   }
-  
+
   void deleteNotes(int index) {
     String id = _notes[index].id;
     _notes.remove(_notes[index]);
--- lib/widgets/notes_list.dart
+++ lib/widgets/notes_list.dart
@@ -4,37 +4,75 @@ import 'package:notes_app/screens/add_notes_screen.dart';
 import 'package:notes_app/widgets/note_card.dart';
 import 'package:provider/provider.dart';
 
+import '../model/notes.dart';
+
 class NotesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<NotesData>(
-      builder: (context, data, child) {
-        return data.notesCount > 0 ? ListView.builder(
-            itemCount: data.notesCount,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AddNoteScreen(
-isUpdate: true,
-                  note: data.note[index],
-                ))),
-                child: NoteCard(
-                  title: data.note[index].title,
-                  description: data.note[index].content,
-                  onPress: () {
-                    data.deleteNotes(index);
-                  },
+    return Consumer<NotesData>(builder: (context, data, child) {
+      return ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextField(
+              onChanged: (val) {
+                data.setSearchQuery(val);
+              },
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              cursorColor: Colors.white,
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(
+                  color: Colors.white,
                 ),
-              );
-            }) : Center(
-          child: Text(
-            "No notes yet...",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                    )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                  )
+                )
+              ),
             ),
           ),
-        );
-      },
-    );
+          data.notesCount > 0
+              ? ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: data.getFilteredNotes().length,
+                  itemBuilder: (context, index) {
+                    Notes currentNote = data.getFilteredNotes()[index];
+                    return GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AddNoteScreen(
+                                    isUpdate: true,
+                                    note: currentNote,
+                                  ))),
+                      child: NoteCard(
+                        title: currentNote.title,
+                        description: currentNote.content,
+                        onPress: () {
+                          data.deleteNotes(index);
+                        },
+                      ),
+                    );
+                  })
+              : Center(
+                  child: Text(
+                    "No notes yet...",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+        ],
+      );
+    });
   }
 }
